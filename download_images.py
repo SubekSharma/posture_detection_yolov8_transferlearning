0d035baf06a5ce34ddbd6aaf2a2f840944a30c56
subeksharma <subeksharmaofficial@gmail.com>
2023-07-14 14:11:40 +0545

    yaml file added

--- download_images.py
+++
@@ -1,7 +0,0 @@
-from simple_image_download import simple_image_download as simp
-response = simp.simple_image_download
-keywords = ["male buzz cut","male undercut","male crewcut", 
-	    "hale temple fade", "male pompadour", "male comb over",
-		"male man bun","male long hair"]
-for kw in keywords: 
-	response().download(kw,50)
