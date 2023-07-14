21b228b7e1e96d551c410420367b855d437da9c3
subeksharma <subeksharmaofficial@gmail.com>
2023-07-14 17:18:21 +0545

    final commit

--- try.py
+++ try.py
@@ -1,13 +1,24 @@
-cap = cv2.videoCapture(0)
-while cap.isOpened():
-    ret, frame = cap.read()
+import cv2
+import numpy as np 
+import torch 
 
-    #Make detections 
-    results = model(frame)
+model = torch.load(path= "best.pt",force_reload=True)
+model.evaluate('train\images\IMG_20230714_104442.jpg')
 
-    cv2.imshow('YOLO',np.squeeze(results.render()))
 
-    if cv2.waitkey(10) & 0xFF == ord('q'):
-        break 
-cap.release()
-cv2.destroyAllwindows()
+# cap = cv2.VideoCapture(0)
+# while cap.isOpened():
+#     ret, frame = cap.read()
+    
+#     # Make detections 
+#     results = model(frame)
+    
+#     cv2.imshow('Detections', np.squeeze(results.render()))
+    
+#     if cv2.waitKey(10) & 0xFF == ord('q'):
+#         break
+
+# cap.release()
+# cv2.destroyAllWindows()
+
+# yolo task=detect mode=predict model=best.pt show=True conf=0.5 source=0 line_thickness=1
